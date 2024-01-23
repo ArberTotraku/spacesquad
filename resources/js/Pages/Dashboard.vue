1c36ba4684ba53114167f4de7a709dea725c1dfe
Arber Totraku <arbertotraku@gmail.com>
2024-01-23 22:27:21 +0100

    Todo-App im Dashboard..

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,9 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import Todo from '@/Pages/Todo.vue'; 
+
+
 </script>
 
 <template>
@@ -16,7 +19,25 @@ import { Head } from '@inertiajs/vue3';
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
                 </div>
+        
+
             </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg m-1">
+                    <div class="p-6 text-gray-900">An interesting project for Space Squad</div>
+                </div>
+        
+
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <Todo />
+                    </div>
+                </div>
+            </div>
+
         </div>
     </AuthenticatedLayout>
 </template>
---
+++ resources/js/Pages/Todo.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <h1>My To-Do List</h1>
+      <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add new todo">
+      <ul>
+        <li v-for="todo in todos" :key="todo.id">
+          {{ todo.title }}
+          <button @click="removeTodo(todo.id)">Delete</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTodo: '',
+        todos: [],
+      };
+    },
+    methods: {
+      addTodo() {
+        if (this.newTodo.trim()) {
+          this.todos.push({ id: Date.now(), title: this.newTodo });
+          this.newTodo = '';
+        }
+      },
+      removeTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id);
+      },
+    },
+  };
+  </script>
+  
